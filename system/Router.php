d8d18bfbc65de71a05c391044446c7f6ce261b7d
Aitor González Fernández <reimashi@gmail.com>
2014-11-25 18:51:44 +0100

    Implementación del enrutador para MVC

---
+++ system/Router.php
@@ -0,0 +1,103 @@
+<?php
+if (defined('PINCHOSFW'))
+{
+    require_once (SYSTEM_FOLDER . 'Configuration.php');
+
+    class Router
+    {
+        private $config;
+        private $controller;
+        private $action;
+        private $params;
+
+        public function __construct() {
+            $this->config = Configuration::getInstance();
+
+            if (isset($_SERVER["REQUEST_URI"])) {
+                $uripath = $_SERVER["REQUEST_URI"];
+
+                // Limpiamos la URL
+                if (strrpos($uripath, BASE_PATH)) {
+                    $count = 1;
+                    $uripath = str_replace(BASE_PATH, "", $uripath, $count);
+                }
+
+                if (strrpos($uripath, "index.php")) {
+                    $count = 1;
+                    $uripath = str_replace("index.php", "", $uripath, $count);
+                }
+
+                if (strrpos($uripath, "?")) {
+                    $uripath = substr($uripath, 0, strrpos($uripath, "?"));
+                }
+
+                // Obtenemos los path's
+                $path = explode("/", trim($uripath, "/"));
+
+                if (isset($path[0]) && $path[0] != "") {
+                    $this->controller = $path[0];
+                }
+                else {
+                    $this->controller = $this->config->default_controller;
+                }
+
+                if (isset($path[1]) && $path[1] != "") {
+                    $this->action = $path[1];
+                }
+                else {
+                    $this->action = 'index';
+                }
+
+                $params = isset($_GET) && is_array($_GET) ? $_GET : array();
+
+                if (count($path) > 2) {
+                    $params = array_merge($params, array_slice($params, 2));
+                }
+            }
+            else {
+                trigger_error('El router no ha podido establecer la ruta.', E_USER_ERROR);
+            }
+        }
+
+        public function handle() {
+            $controllername = ucfirst(strtolower($this->controller));
+            $controllerfile = APP_FOLDER . "controllers/" . $controllername . '.php';
+
+            if (file_exists($controllerfile)) {
+                require_once($controllerfile);
+
+                if (class_exists($controllername) &&
+                    is_subclass_of($controllername, 'Controller'))
+                {
+                    if (method_exists($controllername, $this->action))
+                    {
+                        $minstance = new $controllername();
+                        $minstance->$this->action($this->params);
+                        return TRUE;
+                    }
+                    else
+                    {
+                        trigger_error('El controlador indicado no posee un metodo (' . $this->action . ')', E_USER_ERROR);
+                        return FALSE;
+                    }
+                }
+                else
+                {
+                    trigger_error('No existe un controlador llamado (' . $controllername . ') o no hereda de Controller.', E_USER_ERROR);
+                    return FALSE;
+                }
+            }
+            else
+            {
+                trigger_error('No se ha encontrado el controlador indicado en (' . $controllerfile . ')', E_USER_ERROR);
+                return FALSE;
+            }
+        }
+    };
+
+}
+else
+{
+    header("HTTP/1.0 404 Not Found");
+}
+?>
