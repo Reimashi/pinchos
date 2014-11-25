1ece0531c75ff5629e98ddae8046016392ee92a7
Aitor González Fernández <reimashi@gmail.com>
2014-11-25 17:17:34 +0100

    Archivo de configuración de la base de datos de ejemplo.

---
+++ app/config/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Archivo de configuración de la base de datos */
+
+$config = array();
+
+$config['dbhost'] = 'localhost';
+$config['dbuser'] = 'testuser';
+$config['dbpass'] = 'testpass';
+$config['dbname'] = 'pinchosdb';
