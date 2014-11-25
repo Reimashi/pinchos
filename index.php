2685146a3bfb1813dc53f4d910e3eca73b98ec66
Aitor González Fernández <reimashi@gmail.com>
2014-11-25 18:50:45 +0100

    Añadidas constantes globales

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
 
     // Constantes globales para control de rutas y enlaces
     $rootpath = pathinfo($_SERVER['SCRIPT_FILENAME']);
-    $basepath = basename($rootpath['dirname']);
     $httpprotocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'https://' : 'http://';
 
-    define ("APP_FOLDER", realpath(dirname(__DIR__)) . "/app/");
-    define ("SYSTEM_FOLDER", realpath(dirname(__DIR__)) . "/system/");
-    define ("SITE_URL", $httpprotocol . $_SERVER['HTTP_HOST'] .'/'. $basepath);
+    define ("BASE_PATH", basename($rootpath['dirname']));
+    define ("APP_FOLDER", realpath(dirname(BASE_PATH)) . "/app/");
+    define ("SYSTEM_FOLDER", realpath(dirname(BASE_PATH)) . "/system/");
+    define ("SITE_URL", $httpprotocol . $_SERVER['HTTP_HOST'] .'/'. BASE_PATH);
 
     // Cargamos el router
+    require_once (SYSTEM_FOLDER . 'Router.php');
     $router = new Router();
     $router->handle();
 ?>
