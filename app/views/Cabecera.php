9596e6886a710a0f5dde266a21c2bd9c0c02850d
Aitor González Fernández <reimashi@gmail.com>
2014-11-26 09:43:27 +0100

    Añadidas las vistas de cabecera y pié generales

---
+++ app/views/Cabecera.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>PinChos</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="<?php echo RESOURCES_URL ?>styles/common.css">
+</head>
+
+<body>
+  <div id="head-container">
+    <div id="head-contw">
+      <div id="head-title">
+        <img src="<?php echo RESOURCES_URL ?>img/logo.png" alt="PinChos CMS Logo">
+      </div>
+      <div id="head-menu">
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Participantes</a></li>
+          <li><a href="#">Bases</a></li>
+          <li><a href="#">Agenda</a></li>
+          <li><a href="#"><img src="<?php echo RESOURCES_URL ?>img/menu.png" alt="Menu"></a></li>
+        </ul>
+      </div>
+      <div class="clear-float"></div>
+    </div>
+  </div>
+  <div id="body-container">
---
+++ app/views/Pie.php
@@ -0,0 +1,6 @@
+        </div>
+        <div id="footer-container">
+
+        </div>
+    </body>
+</html>
