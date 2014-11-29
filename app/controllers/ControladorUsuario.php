3a91e8fab9f6f9dfc2cd48b7e35770ad8aeeefef
Aitor González Fernández <reimashi@gmail.com>
2014-11-29 19:16:49 +0100

    Renombrado controlador de usuario para cumplir con la definicion formal

---
+++ app/controllers/ControladorUsuario.php
@@ -0,0 +1,47 @@
+<?php
+if (defined('PINCHOSFW'))
+{
+    require_once (SYSTEM_FOLDER . 'Controller.php');
+
+    class ControladorUsuario extends Controller {
+        /**
+         * Registra un nuevo usuario en el sistema.
+         */
+        public function registrarUsuario ($params) {
+            $configvista = array('body-containers' => array());
+
+            if (!isset($params['form']) && $params['form'] = '') {
+            }
+            $this->render('FormularioRegistrar', null, true);
+
+            $this->render('Principal', $configvista);
+        }
+
+        /**
+        * Registra un nuevo usuario en el sistema.
+        */
+        public function borrarUsuario ($params) {
+            trigger_error('Metodo no implementado.', E_USER_ERROR);
+        }
+
+        /**
+        * Obtiene y muestra un usuario del sistema.
+        */
+        public function obtenerUsuario ($params) {
+            trigger_error('Metodo no implementado.', E_USER_ERROR);
+        }
+
+        /**
+        * Obtiene y muestra un usuario del sistema.
+        */
+        private function validarDatosUsuario ($usuario) {
+            trigger_error('Metodo no implementado.', E_USER_ERROR);
+        }
+    };
+
+}
+else
+{
+    header("HTTP/1.0 404 Not Found");
+}
+?>
