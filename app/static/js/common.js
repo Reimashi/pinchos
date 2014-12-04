ba098a0b814f38d52423c8a8a4ee36f44b5d50c6
Aitor González Fernández <reimashi@gmail.com>
2014-12-04 18:57:15 +0100

    Añadido cacheos de tipo String

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,8 +1,14 @@
+function strTrim(str) {
+    var strcatch = String(str);
+    return strcatch.replace(/^\s+|\s+$/gm,'');
+}
+
 function validateEmail (email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
 
 function stringEmpty (str) {
-    return str.trim().length == 0;
+    var strcatch = String(str);
+    return strTrim(strcatch).length == 0;
 }
