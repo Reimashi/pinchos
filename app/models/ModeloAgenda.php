826729c81803e6b76cbc6b7ab80a55b7d3d5d60b
Aitor González Fernández <reimashi@gmail.com>
2014-12-05 22:19:27 +0100

    Corregida la consulta de la agenda

--- app/models/ModeloAgenda.php
+++ app/models/ModeloAgenda.php
@@ -1,34 +1,33 @@
 <?php
 if (defined('PINCHOSFW'))
 {
-  require_once (SYSTEM_FOLDER . 'Model.php');
+    require_once (SYSTEM_FOLDER . 'Model.php');
 
-  class ModeloAgenda extends Model {
-    /**
-    * Consulta la agenda de un concurso en la base de datos.
-    */
-    public function consultarAgenda ($idconcurso=1) {
-        $qresult = $this->db->query("SELECT nombre, descripcion, fecha_inicio, fecha_fin FROM agenda WHERE id_concurso=1");
+    class ModeloAgenda extends Model {
+        /**
+        * Consulta la agenda de un concurso en la base de datos.
+        */
+        public function consultarAgenda ($idconcurso=1) {
+            $qresult = $this->db->query("SELECT nombre, descripcion, fecha_inicio, fecha_fin FROM agenda WHERE id_concurso=1");
 
-        if ($qresult && $qresult->num_rows > 0) {
-            $agenda = array();
+            if ($qresult && $qresult->num_rows > 0) {
+                $agenda = array();
 
-            // Se recorren las filas encontradas en la base de datos
-            while ($entradaagenda = $qresult->fetch_assoc()) {
-                $agenda[] = $entradaagenda;
+                // Se recorren las filas encontradas en la base de datos
+                while ($entradaagenda = $qresult->fetch_assoc()) {
+                    $agenda[] = $entradaagenda;
+                }
+
+                return $agenda;
             }
-
-            return $agenda;
+            else {
+                return FALSE;
+            }
         }
-        else {
-            return FALSE;
-        }
-    }
-  };
-
+    };
 }
 else
 {
-  header("HTTP/1.0 404 Not Found");
+    header("HTTP/1.0 404 Not Found");
 }
 ?>
