14c1efaf57b3c6aa5625ec2a698c7d1203ddfca0
Aitor González Fernández <reimashi@gmail.com>
2014-12-05 21:48:16 +0100

    Arreglado modelo concurso

--- app/models/ModeloConcurso.php
+++ app/models/ModeloConcurso.php
@@ -1,55 +1,27 @@
 <?php
 if (defined('PINCHOSFW'))
 {
-  require_once (SYSTEM_FOLDER . 'Model.php');
+    require_once (SYSTEM_FOLDER . 'Model.php');
 
-  class ModeloConcurso extends Model {
-    /**
-    * Consulta las bases de un concurso en la base de datos.
-    */
-    public function consultarBases ($idconcurso) {
+    class ModeloConcurso extends Model {
+        /**
+        * Consulta las bases de un concurso en la base de datos.
+        */
+        public function consultarBases ($idconcurso) {
 
-      $qresult = $this->db->query("SELECT bases FROM concursos WHERE id=1");
+            $qresult = $this->db->query("SELECT bases FROM concursos WHERE id=1");
 
-      if ($qresult && $qresult->num_rows > 0) {
-        $bases = array();
-
-        // Se recorren las filas encontradas en la base de datos
-        while ($entradabases = $qresult->fetch_assoc()) {
-          $bases[] = $entradabases;
+            if ($qresult && $qresult->num_rows == 1) {
+                return $qresult->fetch_assoc()['bases'];
+            }
+            else {
+                return FALSE;
+            }
         }
-
-        return $bases;
-      }
-      else {
-        return FALSE;
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-  };
+    };
 }
 else
 {
-  header("HTTP/1.0 404 Not Found");
+    header("HTTP/1.0 404 Not Found");
 }
 ?>
