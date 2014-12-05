ad8e818f59ca6dbadcb1ccfce24e7e9c012b076b
Aitor González Fernández <reimashi@gmail.com>
2014-12-05 23:27:45 +0100

    Implementado consultarPremios

--- app/models/ModeloPremios.php
+++ app/models/ModeloPremios.php
@@ -3,18 +3,32 @@ if (defined('PINCHOSFW'))
 {
     require_once (SYSTEM_FOLDER . 'Model.php');
 
-    class ModeloConcurso extends Model {
+    class ModeloPremios extends Model {
         /**
         * Consulta los premios de un concurso en la base de datos.
         */
         public function consultarPremios ($idconcurso) {
-            trigger_error('Metodo no implementado.', E_USER_ERROR);
+            $qresult = $this->db->query("SELECT * FROM premios WHERE id_concurso = " . $idconcurso);
+
+            if ($qresult && $qresult->num_rows > 0) {
+                $premios = array();
+
+                // Se recorren las filas encontradas en la base de datos
+                while ($premio = $qresult->fetch_assoc()) {
+                    $premios[] = $premio;
+                }
+
+                return $premios;
+            }
+            else {
+                return FALSE;
+            }
         }
 
         /**
         * Consulta la información de un premio en la base de datos.
         */
-        public function consultarPremio ($idconcurso, $nombrepremio) {
+        public function consultarPremio ($idconcurso, $idpremio) {
             trigger_error('Metodo no implementado.', E_USER_ERROR);
         }
     };
