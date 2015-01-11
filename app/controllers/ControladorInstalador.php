162c589c4cee6dc6523bd36573f94bafdbb66772
Aitor González Fernández <reimashi@gmail.com>
2015-01-11 20:37:11 +0100

    Añadidos metodos para crear archivos de configuracion e importar .sql.

--- app/controllers/ControladorInstalador.php
+++ app/controllers/ControladorInstalador.php
@@ -14,7 +14,66 @@ if (defined('PINCHOSFW'))
             // Comprobacion de seguridad.
             if (!defined('INSTALL_MODE')) { header("HTTP/1.1 401 Unauthorized"); return; }
 
-            echo 'Instalacion';
+            if(isset($params['post']['form-name']) && $params['post']['form-name'] = 'install-info') {
+
+            }
+            else {
+
+            }
+        }
+
+        private function import_sql($filename, $mysql_host, $mysql_username, $mysql_password, $mysql_database) {
+            if (file_exists($filename)) {
+                if (!mysql_connect($mysql_host, $mysql_username, $mysql_password)) {
+                    return FALSE;
+                }
+
+                if (!mysql_select_db($mysql_database)) {
+                    return FALSE;
+                }
+
+                $templine = '';
+                $lines = file($filename);
+
+                foreach ($lines as $line)
+                {
+                    // Evitamos lineas vacias y comentarios
+                    if (substr($line, 0, 2) == '--' || $line == '')
+                    continue;
+
+                    $templine .= $line;
+
+                    // Si la sentencia SQL esta completa, la ejecutamos.
+                    if (substr(trim($line), -1, 1) == ';')
+                    {
+                        if (!mysql_query($templine)) {
+                            return FALSE;
+                        }
+                        $templine = '';
+                    }
+                }
+
+                return TRUE;
+            }
+            else {
+                return FALSE;
+            }
+        }
+
+        private function generate_conf_file($filename, $config) {
+            $path = APP_FOLDER . 'config/' . ucfirst(strtolower($filename)) . '.php';
+
+            if ($myfile = fopen($path, "w")) {
+                if (!fwrite($myfile, "<?php\n\$config = array();\n")) return FALSE;
+                foreach ($config as $clave => $valor) {
+                    if (!fwrite($myfile, "\$config[\"" . $clave . "\"] = \"" . $valor . "\";")) return FALSE;
+                }
+                if (!fwrite($myfile, "\n?>")) return FALSE;
+                return fclose($myfile);
+            }
+            else {
+                return FALSE;
+            }
         }
     };
 }
