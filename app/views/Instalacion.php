fc2bc1f7a202c13e326d53dd7b62bf5a1b7f35ab
Aitor González Fernández <reimashi@gmail.com>
2015-01-11 20:37:34 +0100

    Añadida vista preliminar de instalación

---
+++ app/views/Instalacion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Instalador de PinChos</title>
+    </head>
+    <body>
+        <h2>Instalador de PinChos</h2>
+        <h4>Base de datos</h4>
+        <table>
+            <tr>
+                <td>Usuario:</td>
+                <td><input type="text" name="dbuser"/></td>
+            </tr>
+            <tr>
+                <td>Contraseña:</td>
+                <td><input type="text" name="dbuser"/></td>
+            </tr>
+            <tr>
+                <td>Dirección:</td>
+                <td><input type="text" name="dbuser"/></td>
+            </tr>
+            <tr>
+                <td>Tabla:</td>
+                <td><input type="text" name="dbuser"/></td>
+            </tr>
+        </table>
+        <h4>Consurso</h4>
+        <table>
+            <tr>
+                <td>Nombre:</td>
+                <td><input type="text" name="dbuser"/></td>
+            </tr>
+        </table>
+    </body>
+</html>
