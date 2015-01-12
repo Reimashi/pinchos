f1bde35cf6b606196c5308dfbf5e44d540a7c108
Aitor González Fernández <reimashi@gmail.com>
2015-01-12 10:37:41 +0100

    Implementado el script de instalacion

--- app/controllers/ControladorInstalador.php
+++ app/controllers/ControladorInstalador.php
@@ -14,21 +14,96 @@ if (defined('PINCHOSFW'))
             // Comprobacion de seguridad.
             if (!defined('INSTALL_MODE')) { header("HTTP/1.1 401 Unauthorized"); return; }
 
-            if(isset($params['post']['form-name']) && $params['post']['form-name'] = 'install-info') {
-
+            if(isset($params['post']['form-name']) && $params['post']['form-name'] == 'install-info') {
+                $form_error = "";
+                
+                if (!isset($params['post']['dbuser']) || $params['post']['dbuser'] == "") {
+                    $form_error .= 'No se ha pasado el usuario de la base de datos<br>';
+                }
+                if (!isset($params['post']['dbpass']) || $params['post']['dbpass'] == "") {
+                    $form_error .= 'No se ha pasado la contraseña de la base de datos<br>';
+                }
+                if (!isset($params['post']['dbhost']) || $params['post']['dbhost'] == "") {
+                    $form_error .= 'No se ha pasado la dirección de la base de datos<br>';
+                }
+                if (!isset($params['post']['dbname']) || $params['post']['dbname'] == "") {
+                    $form_error .= 'No se ha pasado el nombre de la base de datos<br>';
+                }
+                if (!isset($params['post']['concurname']) || $params['post']['concurname'] == "") {
+                    $form_error .= 'No se ha pasado el nombre del concurso<br>';
+                }
+                
+                if ($form_error != "") {
+                    return $this->render('Instalacion', array('form-error' => $form_error));
+                }
+                
+                if (!$this->test_sql_connection($params['post']['dbhost'],  
+                        $params['post']['dbuser'], 
+                        $params['post']['dbpass'], 
+                        $params['post']['dbname'])) {
+                    return $this->render('Instalacion', array('form-error' => "No se ha podido conectar con la base de datos. Configuración incorrecta.<br>"));
+                }
+                else {
+                    $config = array();
+                    $config['dbhost'] = $params['post']['dbhost'];
+                    $config['dbuser'] = $params['post']['dbuser'];
+                    $config['dbpass'] = $params['post']['dbpass'];
+                    $config['dbname'] = $params['post']['dbname'];
+                    if (!$this->generate_conf_file("Database", $config)) {
+                        return $this->render('Instalacion', array('form-error' => "No se ha podido crear el archivo de configuracion. Comprueba que PHP tiene permisos de escritura.<br>"));
+                    }
+                }
+                
+                if(!$this->import_sql(APP_FOLDER . 'create.sql',
+                        $params['post']['dbhost'],  
+                        $params['post']['dbuser'], 
+                        $params['post']['dbpass'], 
+                        $params['post']['dbname'])) {
+                    $form_error .= "WARNING: No se ha podido importar el script .sql . Importelo manualmente.<br>";
+                }
+                
+                $this->config->reload();
+                
+                $modeloConcurso = $this->loadModel('Concurso');
+                // Sin soporte para multiples concursos, concurso siempre será 1
+                if (!$modeloConcurso->crearConcurso(array("nombre" => $params['post']['concurname']))) {
+                    $form_error .= "WARNING: No se ha podido crear el concurso en la base de datos. Intentelo manualmente.<br>";
+                }
+                
+                $form_error .= "Instalación completada!<br>";
+                
+                return $this->render('Instalacion', array('form-error' => $form_error));
             }
             else {
-
+                $this->render('Instalacion', null);
             }
+        }
+        
+        private function test_sql_connection($mysql_host, $mysql_username, $mysql_password, $mysql_database) {
+            $res = mysql_connect($mysql_host, $mysql_username, $mysql_password);
+            if (!$res) {
+                echo "Conexion incorrecta";
+                return FALSE;
+            }
+
+            if (!mysql_select_db($mysql_database, $res)) {
+                echo "Tabla incorrecta";
+                return FALSE;
+            }
+            
+            mysql_close ($res);
+            
+            return TRUE;
         }
 
         private function import_sql($filename, $mysql_host, $mysql_username, $mysql_password, $mysql_database) {
             if (file_exists($filename)) {
-                if (!mysql_connect($mysql_host, $mysql_username, $mysql_password)) {
+                $res = mysql_connect($mysql_host, $mysql_username, $mysql_password);
+                if (!$res) {
                     return FALSE;
                 }
 
-                if (!mysql_select_db($mysql_database)) {
+                if (!mysql_select_db($mysql_database, $res)) {
                     return FALSE;
                 }
 
@@ -46,13 +121,14 @@ if (defined('PINCHOSFW'))
                     // Si la sentencia SQL esta completa, la ejecutamos.
                     if (substr(trim($line), -1, 1) == ';')
                     {
-                        if (!mysql_query($templine)) {
+                        if (!mysql_query($templine, $res)) {
                             return FALSE;
                         }
                         $templine = '';
                     }
                 }
 
+                mysql_close ($res);
                 return TRUE;
             }
             else {
@@ -63,12 +139,18 @@ if (defined('PINCHOSFW'))
         private function generate_conf_file($filename, $config) {
             $path = APP_FOLDER . 'config/' . ucfirst(strtolower($filename)) . '.php';
 
-            if ($myfile = fopen($path, "w")) {
-                if (!fwrite($myfile, "<?php\n\$config = array();\n")) return FALSE;
-                foreach ($config as $clave => $valor) {
-                    if (!fwrite($myfile, "\$config[\"" . $clave . "\"] = \"" . $valor . "\";")) return FALSE;
+            if (($myfile = fopen($path, "w"))) {
+                if (!fwrite($myfile, "<?php\n\$config = array();\n")) {
+                    return FALSE;
                 }
-                if (!fwrite($myfile, "\n?>")) return FALSE;
+                foreach ($config as $clave => $valor) {
+                    if (!fwrite($myfile, "\$config[\"" . $clave . "\"] = \"" . $valor . "\";\n")) {
+                        return FALSE;
+                    }
+                }
+                if (!fwrite($myfile, "\n?>")) {
+                    return FALSE;
+                }
                 return fclose($myfile);
             }
             else {
--- app/views/Instalacion.php
+++ app/views/Instalacion.php
@@ -5,6 +5,8 @@
     </head>
     <body>
         <h2>Instalador de PinChos</h2>
+        <form name="install-info" action="<?php echo SITE_URL; ?>/index.php" method="POST">
+        <input type="hidden" name="form-name" value="install-info">
         <h4>Base de datos</h4>
         <table>
             <tr>
@@ -13,23 +15,32 @@
             </tr>
             <tr>
                 <td>Contraseña:</td>
-                <td><input type="text" name="dbuser"/></td>
+                <td><input type="text" name="dbpass"/></td>
             </tr>
             <tr>
                 <td>Dirección:</td>
-                <td><input type="text" name="dbuser"/></td>
+                <td><input type="text" name="dbhost"/></td>
             </tr>
             <tr>
                 <td>Tabla:</td>
-                <td><input type="text" name="dbuser"/></td>
+                <td><input type="text" name="dbname"/></td>
             </tr>
         </table>
         <h4>Consurso</h4>
         <table>
             <tr>
                 <td>Nombre:</td>
-                <td><input type="text" name="dbuser"/></td>
+                <td><input type="text" name="concurname"/></td>
             </tr>
         </table>
+        <br>
+        <?php 
+        if (isset($params['form-error'])) {
+            echo $params['form-error'];
+            echo '<br>';
+        }
+        ?>
+        <input type="submit" value="Instalar"/>
+        </form>
     </body>
 </html>
